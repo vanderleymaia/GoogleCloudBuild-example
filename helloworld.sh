73ddb69d2b96f012e285c6349eb6caaa54c427d9
Vanderley Maia <vanderleymaia@gmail.com>
2020-09-21 20:13:35 -0300

    ok

--- helloworld.sh
+++ helloworld.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Hello, world!"
+echo "Hello, world Maia!"
 echo "The time is $(date)."
